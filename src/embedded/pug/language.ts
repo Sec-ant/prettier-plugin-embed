a8e0030cdacb91fabd88f04a85b27e2ef5460475
Ze-Zheng Wu <zezhengwu@proton.me>
2024-01-22 03:13:33 +0800

    feat: support `pug`
    uses `@prettier/plugin-pug`

---
+++ src/embedded/pug/language.ts
@@ -0,0 +1,10 @@
+export const language = "embeddedPug";
+
+/**
+ * Register the language to the EmbeddedLanguagesHolder
+ */
+declare module "../types.js" {
+  interface EmbeddedLanguagesHolder {
+    [language]: void;
+  }
+}
