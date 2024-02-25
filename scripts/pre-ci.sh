409949aa3a3070f654815fc93d9e5b9773217a14
Ze-Zheng Wu <zezhengwu@proton.me>
2024-02-25 19:48:49 +0800

    chore: use `jq`

--- scripts/pre-ci.sh
+++ scripts/pre-ci.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-BIOME_VERSION=$(node -p "require('@biomejs/biome/package.json').version")
+curl -L https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64 -o jq
+chmod a+x jq
+
+BIOME_VERSION=$(npm -j ls | ./jq -r '.dependencies["@biomejs/biome"].version')
 npm i -DE biome-cli-codesandbox@$BIOME_VERSION
 
-npm pkg set scripts.lint="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts.lint | sed -e 's/^"//' -e 's/"$//')"
-npm pkg set scripts['format:biome']="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts['format:biome'] | sed -e 's/^"//' -e 's/"$//')"
-npm pkg set scripts['check:biome']="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts['check:biome'] | sed -e 's/^"//' -e 's/"$//')"
+npm pkg set scripts.lint="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts.lint | ./jq -r)"
+npm pkg set scripts['format:biome']="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts['format:biome'] | ./jq -r)"
+npm pkg set scripts['check:biome']="BIOME_BINARY=biome-cli-codesandbox/biome $(npm pkg get scripts['check:biome'] | ./jq -r)"
+
+rm jq
